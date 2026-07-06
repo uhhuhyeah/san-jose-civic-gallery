a8fb432fa38ed420d73a39f2230b1410abade7dd
David A McClain <mail@davidamcclain.com>
2026-07-06 12:58:51 -0700

    Sentry configuration hygiene

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -4,11 +4,19 @@ Sentry.init do |config|
   config.dsn = ENV["SENTRY_DSN"]
 
   config.breadcrumbs_logger = [ :active_support_logger, :http_logger ]
-  config.send_default_pii = true
+  config.send_default_pii = false
 
   config.enable_logs = true
   config.enabled_patches = [ :logger ]
 
-  config.traces_sample_rate = 1.0
-  config.profiles_sample_rate = 1.0
+  config.traces_sample_rate = 0.1
+  config.profiles_sample_rate = 0.1
+
+  config.before_send_transaction = lambda do |event, _hint|
+    if event.request&.url&.include?("/up")
+      nil
+    else
+      event
+    end
+  end
 end
