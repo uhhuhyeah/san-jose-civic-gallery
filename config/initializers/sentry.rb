4f9229f0c929279a618c385a2149453e2e6c2d0c
David A McClain <mail@davidamcclain.com>
2026-07-15 12:53:29 -0700

    Disable Sentry log ingestion

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -6,11 +6,11 @@ Sentry.init do |config|
   config.breadcrumbs_logger = [ :active_support_logger, :http_logger ]
   config.send_default_pii = false
 
-  config.enable_logs = true
-  config.enabled_patches = [ :logger ]
+  config.enable_logs = false
+  config.enabled_patches = []
 
-  config.traces_sample_rate = 0.1
-  config.profiles_sample_rate = 0.1
+  config.traces_sample_rate = 0.01
+  config.profiles_sample_rate = 0.0
 
   config.before_send_transaction = lambda do |event, _hint|
     if event.request&.url&.include?("/up")
